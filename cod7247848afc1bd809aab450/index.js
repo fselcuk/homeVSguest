7493538e11b2af22799cd41c1f04d7471789697e
fselcuk <fehmiselcuk06@gmail.com>
2022-08-18 23:05:48 +0300

    first commit

---
+++ cod7247848afc1bd809aab450/index.js
@@ -0,0 +1,39 @@
+let scoreHome = 0
+
+
+function hIncBy1(){
+    scoreHome += 1
+    document.getElementById("scorehome").textContent=scoreHome
+}
+
+function hIncBy2(){
+    scoreHome+=2
+    document.getElementById("scorehome").textContent=scoreHome
+}
+
+
+function hIncBy3(){
+    scoreHome+=3
+    document.getElementById("scorehome").textContent=scoreHome
+
+}
+
+let scoreGuest = 0
+
+
+function gIncBy1(){
+    scoreGuest += 1
+    document.getElementById("scoreguest").textContent=scoreGuest
+}
+
+function gIncBy2(){
+    scoreGuest+=2
+    document.getElementById("scoreguest").textContent=scoreGuest
+}
+
+
+function gIncBy3(){
+    scoreGuest+=3
+    document.getElementById("scoreguest").textContent=scoreGuest
+
+}
---
+++ index.js
@@ -0,0 +1,20 @@
+let scoreHome=0
+
+
+function hIncBy1(){
+    scoreHome+=1
+    document.getElementById("scorehome").textContent=scoreHome
+}
+
+function hIncBy2(){
+    scoreHome+=2
+    document.getElementById("scorehome").textContent=scoreHome
+}
+
+
+function hIncBy3(){
+    scoreHome+=3
+    document.getElementById("scorehome").textContent=scoreHome
+
+}
+
